f1f27e00f04b5d907f182d4c48c1b8ff446e869b
Justus Walter-Helk <75504335+JustusWalter-Helk@users.noreply.github.com>
2023-09-19 17:34:59 +0200

    Emptied Project
    Emptied Project for major restructuring.

--- kyuubiforge/src/main/java/kyuubiforge/Core/Window/GeneralWindowHandler.java
+++
@@ -1,7 +0,0 @@
-package kyuubiforge.Core.Window;
-
-public class GeneralWindowHandler extends AbstractWindowHandler{
-    public GeneralWindowHandler(Window window, WindowSpecification windowSpecification) {
-        super(window, windowSpecification);
-    }
-}
